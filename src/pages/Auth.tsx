3f5a81433c769ff0638af1e9094232119e11d2cc
febrd <febridirgantara17@gmail.com>
2025-07-20 03:19:02 +0700

    deksripsition ajdusmetn

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -45,8 +45,8 @@ const Auth = () => {
           <CardContent>
             <Tabs defaultValue="login" className="w-full">
               <TabsList className="grid w-full grid-cols-2">
-                <TabsTrigger value="login">Masuk</TabsTrigger>
-                <TabsTrigger value="register">Daftar</TabsTrigger>
+                <TabsTrigger value="login">Sudah Pernah Masuk</TabsTrigger>
+                <TabsTrigger value="register">Belum Pernah Masuk</TabsTrigger>
               </TabsList>
 
               {error && (
