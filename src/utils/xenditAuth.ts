236277cebe0facb0f0366333639f91c3c82f5a18
Febriansah Dirgantara <febridirgantara17@gmail.com>
2025-07-31 20:05:51 +0700

    Update xenditAuth.ts - hilangkan xendit /v2/ karna default api url adalah https://api.xendit.co/v2/
    hilangkan xendit /v2/ karna default api url adalah https://api.xendit.co/v2/

--- src/utils/xenditAuth.ts
+++ src/utils/xenditAuth.ts
@@ -44,7 +44,7 @@ export class XenditAuthClient {
     try {
       console.log('Testing Xendit connection to:', this.apiUrl);
       
-      const response = await this.makeRequest('/v2/invoices?limit=1', {
+      const response = await this.makeRequest('invoices?limit=1', {
         method: 'GET',
       });
       
@@ -83,7 +83,7 @@ export class XenditAuthClient {
     try {
       console.log('Creating invoice with data:', invoiceData);
       
-      const response = await this.makeRequest('/v2/invoices', {
+      const response = await this.makeRequest('invoices', {
         method: 'POST',
         body: JSON.stringify(invoiceData)
       });
