d7dc1010fd99d3024c988725e69698aa7cb041d9
febrd <febridirgantara17@gmail.com>
2025-07-31 16:13:25 +0700

    changes favicon supabase client server

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 // Ganti dengan URL Supabase kamu (self-hosted)
-const SUPABASE_URL = "http://core.masukstudio.com";
+const SUPABASE_URL = "https://core.masukstudio.com";
 
 // Ganti dengan ANON_KEY dari .env kamu
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
